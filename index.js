dd2b9d3fc7cccff639bdf803fe397b954c2db6b8
stanimirovv <zlatin.stanimirovv@gmail.com>
2020-03-08 22:38:38 +0200

    first 'working' version, needs qa

--- index.js
+++ index.js
@@ -8,71 +8,31 @@ const pages = require('./lib/pages');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json())
 pages.setApp(app);
-/*
- * IN: noteId
- * IN: authToken
- * OUT: status, encrypted payload
- *
- * IN: noteId
- * IN: wrong or missing authToken
- * OUT: Redirect to /n/noteId/auth
- *
- * IN: unexisting noteId in the system
- * IN: unexisting authToken
- * OUT: create new note, session and load the UI. Prompt first save
- *
- */
+
 app.get('/n/:hash', (req, res) => {
-    console.log(req.params);
-    // console.log(req.query.kakaka);
     let myNote = note.getNote(req.params.hash);
-    if (myNote === undefined) {
-        res.render('index', {encryptedContent: false, integrity: note.getIntegrityWord()});
+    if (myNote === null) {
+        res.render('index', {encryptedContent: false, noteExists: false, errorMessage: 'System error'});
         return;
     }
-    res.render('index', {encryptedContent: myNote.body, integrity: note.getIntegrityWord()});
+    // New note
+    if (myNote === undefined) {
+        res.render('index', {encryptedContent: false, noteExists: false, errorMessage: false});
+        return;
+    }
+    // Existing note
+    res.render('index', {encryptedContent: myNote.body, noteExists: true, errorMessage: false});
 });
 
-/*
- * IN: noteId
- * IN: privateKey
- * OUT: redirect to /n/:noteId with Auth
- * NOTE: opens up page with prompt to enter key; when ok is sent, the page sends request to
- */
-app.get('/n/:hash/auth', (req, res) => { 
-    console.log(req.params);
-    res.send('Hello World!');
-});
-
-/* IN: noteId
- * IN: encryped msg 'OK'
- * OUT: authToken
- *
- * IN: noteId
- * IN: wrong or missing encrypted msg
- * OUT: error message to display in the UI
- *
- * IN: wrong noteId
- * IN: encryped msg 'OK'
- * OUT: error message to display in the UI
- */
-app.post('/n/:hash/auth', (req, res) => {
-    console.log(req.params);
-    res.send('Hello World!');
-});
-
-/* IN: noteId
- * IN: authToken
- * OUT: ok msg for the client
- *
- * ANY of the arguments wrong -> error msg for the client
- */
 app.post('/n/:hash/save', (req, res) => {
     console.log(req.body);
     console.log(req.params);
-    note.saveNote(req.params.hash, 'asd', req.body);
+    let status = note.saveNote(req.params.hash, req.body);
+    if (status === null) {
+        res.send('{"status" : "error"}');
+    }
 
-    res.send('{"asd":123}');
+    res.send('{"status":"ok"}');
 });
 
 app.use('/public', express.static('public'));
--- initdb.js
+++ initdb.js
@@ -1,6 +1,5 @@
 const db = require('better-sqlite3')('test.db', {});
 db.prepare('CREATE TABLE notes(id INTEGER PRIMARY KEY AUTOINCREMENT, ' + 
             'hash varchar(256) NOT NULL, ' +
-            'integrity varchar(256) NOT NULL, ' +
             'body TEXT NOT NULL);'
           ).run();
