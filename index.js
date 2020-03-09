081ade602f817d6a864459b9efb61bc38cbaf923
stanimirovv <zlatin.stanimirovv@gmail.com>
2020-03-10 00:44:36 +0200

    adds logs

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-const note = require('./lib/note');
-const pages = require('./lib/pages');
 
+const note = require('./lib/note');
+const log = require('./lib/log');
+
+const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.json())
-pages.setApp(app);
 
 app.get('/n/:hash', (req, res) => {
-    let myNote = note.getNote(req.params.hash);
+    let hash = req.params.hash;
+    let myNote = note.getNote(hash);
+
+    // Error fetching note
     if (myNote === null) {
+        log.error('Error getting noteId: ' + hash);
         res.render('index', {encryptedContent: false, noteExists: false, errorMessage: 'System error'});
         return;
     }
+
     // New note
     if (myNote === undefined) {
+        log.info('Note is free noteId: ' + hash);
         res.render('index', {encryptedContent: false, noteExists: false, errorMessage: false});
         return;
     }
+
     // Existing note
+    log.info('Note exists noteId: ' + hash);
     res.render('index', {encryptedContent: myNote.body, noteExists: true, errorMessage: false});
 });
 
@@ -29,12 +36,14 @@ app.post('/n/:hash/save', (req, res) => {
     console.log(req.params);
     let status = note.saveNote(req.params.hash, req.body);
     if (status === null) {
-        res.send('{"status" : "error"}');
+        res.send(JSON.serialize({"status" : "error"}));
     }
 
-    res.send('{"status":"ok"}');
+    res.send(JSON.serialize({"status":"ok"}));
 });
 
 app.use('/public', express.static('public'));
 
+// TODO config
+const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
