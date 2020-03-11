51d01096c0d33e4e9debb00bd5d2a4d87358b287
stanimirovv <zlatin.stanimirovv@gmail.com>
2020-03-11 23:48:29 +0200

    adds checksum to note

--- initdb.js
+++ initdb.js
@@ -1,5 +1,6 @@
 const db = require('better-sqlite3')('test.db', {});
 db.prepare('CREATE TABLE notes(id INTEGER PRIMARY KEY AUTOINCREMENT, ' + 
             'hash varchar(256) NOT NULL, ' +
+            'checksum varchar(256) NOT NULL, ' +
             'body TEXT NOT NULL);'
           ).run();
